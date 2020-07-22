cc912da10949c1c1916c9e7457a4f67fc02671b5
chliu65 <chang_liu@hnu.edu.cn>
2020-07-22 19:44:21 +0800

    parent|eureka-server|gateway

---
+++ ad-gateway/src/main/java/com/hnu/adspringcloud/AdGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.hnu.adspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class AdGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdGatewayApplication.class, args);
+	}
+
+}
